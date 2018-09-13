f8f211eca3b395dc17520b0b29efe1c8d8f50258
Lucas Spirandeli <lucasspi@yahoo.com.br>
2018-09-13 11:59:18 -0300

    Vetor2D

---
+++ 2DVector/2DVector/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2DVector
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Vector2D vector_1, vector_2;
+
+            vector_1 = new Vector2D(5.0, 9.0);
+
+            vector_2 = new Vector2D();
+            vector_2.set_xy(10.0, 3.0);
+
+            Console.Out.WriteLine("Vetor 1: (" + vector_1.get_x() + ", " + vector_1.get_y() + ")");
+            Console.Out.WriteLine("Vetor 2: (" + vector_2.get_x() + ", " + vector_2.get_y() + ")");
+
+            Console.Out.WriteLine("\nProduto Escalar: " + vector_1.scalar_product(vector_2));
+
+            Console.Out.WriteLine("\nVetor 1 modulo: " + vector_1.module());
+            Console.Out.WriteLine("Vetor 2 modulo: " + vector_2.module());
+
+            Console.Out.WriteLine("\nAngulo entre vetoress:");
+            Console.Out.WriteLine("Radianos = " + vector_1.angle_between(vector_2));
+            Console.Out.WriteLine("Graus = " + vector_1.angle_between(vector_2, degrees: true) + "°");
+
+            Vector2D projection = vector_1.projection(vector_2);
+
+            Console.Out.WriteLine("\nProjection of 1 on 2: (" + projection.get_x() + ", " + projection.get_y() + ")");
+
+
+            Console.In.Read();
+        }
+    }
+}
---
+++ 2DVector/2DVector/Vector2D.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace _2DVector
+{
+    class Vector2D
+    {
+        private double x_coordinate;
+        private double y_coordinate;
+
+        public Vector2D()
+        {
+            x_coordinate = 0;
+            y_coordinate = 0;
+        }
+
+        public Vector2D(double x, double y)
+        {
+            x_coordinate = x;
+            y_coordinate = y;
+        }
+
+        public void set_x(double x)
+        {
+            x_coordinate = x;
+        }
+
+        public void set_y(double y)
+        {
+            y_coordinate = y;
+        }
+
+        public void set_xy(double x, double y)
+        {
+            set_x(x);
+            set_y(y);
+        }
+
+        public double get_x()
+        {
+            return x_coordinate;
+        }
+
+        public double get_y()
+        {
+            return y_coordinate;
+        }
+
+        public double module()
+        {
+            return (Math.Pow((Math.Pow(x_coordinate, 2) +
+                              Math.Pow(y_coordinate, 2)), 0.5));
+        }
+
+        public double scalar_product(Vector2D other_vector)
+        {
+            return (x_coordinate * other_vector.x_coordinate +
+                    y_coordinate * other_vector.y_coordinate);
+        }
+
+        public double angle_between(Vector2D other_vector, bool degrees = false)
+        {
+            double theta;
+            // Cos(Theta) = (u.v) / (||u||*||v||)
+            theta = Math.Acos(scalar_product(other_vector) / (module() * other_vector.module()));
+            return ((degrees) ? theta * 180 / Math.PI : theta);
+        }
+
+        public Vector2D projection(Vector2D other_vector)
+        {
+            double other_module, other_x, other_y, cosine, mod;
+            other_module = other_vector.module();
+            other_x = other_vector.get_x();
+            other_y = other_vector.get_y();
+            cosine = Math.Cos(angle_between(other_vector));
+            mod = module();
+
+            Vector2D unit_vector = new Vector2D(other_x / other_module, other_y / other_module);
+
+            // proj = (|a|*cos(theta))*unit_b
+            return (new Vector2D(mod * cosine * unit_vector.get_x(), mod * cosine * unit_vector.get_y()));
+
+        }
+    }
+}
